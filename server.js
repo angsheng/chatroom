4881f1171712bd1cc45b2f6578fc2215682cf9f4
Ang <349239072@qq.com>
2018-03-31 18:23:29 +0800

    +

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 var ws=require('nodejs-websocket')
 
-
-
 var server = ws.createServer(
 	function(conn){
 		console.log('new connection');
 
 		conn.on('text',function(str){ 
 
-			broadcast(conn,str )
+			broadcast( conn,str )
 		});
 
 		conn.on('close',function(){ console.log('closed this connection ') });
@@ -18,7 +16,8 @@ var server = ws.createServer(
 ).listen(8080)
 
 
-function broadcast(conn , newmessage ){
-	server.connections.forEach( function(conn){conn.sendText(newmessage)} )
+function broadcast(conn , nm){
+	server.connections.forEach( function(conn){conn.sendText( nm )} )  //这种作为参数的函数声明其能看到哪的变量？回调函数的回调参数其能看到哪的变量？
+	//当前调用时候其参数声明在哪就能看到哪，但回调类函数呢？
 }
 
