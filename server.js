94b2f223e451cc1dbd3f36f25f7f36643c1dba65
Ang <349239072@qq.com>
2018-03-31 17:08:15 +0800

    实现聊天室效果，但是并不清楚API

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 var ws=require('nodejs-websocket')
-var server = ws.createServer(function(conn){
-	console.log('new connection.. ');
-	conn.on('text',function(str){
-		console.log( 'recieved: ' + str );
-		conn.sendText( str.toUpperCase() + '!!!')
-	});
-	conn.on('close',function(code,reason){
-		console.log('connetion closed')
-	})
-}).listen(8080)
+
+
+
+var server = ws.createServer(
+	function(conn){
+		console.log('new connection');
+
+		conn.on('text',function(str){ 
+
+			broadcast(conn,str )
+		});
+
+		conn.on('close',function(){ console.log('closed this connection ') });
+
+		conn.on('error',function(){ console.log('Error'); conn.close() })
+	}
+).listen(8080)
+
+
+function broadcast(conn , newmessage ){
+	server.connections.forEach( function(conn){conn.sendText(newmessage)} )
+}
+
