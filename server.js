87eec76947bebe7ba84f375739907d0414171565
Ang <349239072@qq.com>
2018-04-04 22:44:28 +0800

    提交

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ var server = ws.createServer(
 
 
 function broadcast(conn , nm){
-	server.connections.forEach( function(conn){conn.sendText( nm )} )  //这种作为参数的函数声明其能看到哪的变量？回调函数的回调参数其能看到哪的变量？
-	//当前调用时候其参数声明在哪就能看到哪，但回调类函数呢？
+	server.connections.forEach( function(conn){conn.sendText( nm )} )  
 }
 
